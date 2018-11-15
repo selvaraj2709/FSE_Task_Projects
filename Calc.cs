c9886b85ca95be11110e02f5b62de318d7800603
selvaraj2709 <40266974+selvaraj2709@users.noreply.github.com>
2018-11-15 21:55:02 +0530

    FSE Assignment 21
    FSE Assignment 21

---
+++ Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21WCFFSDCalculator
+{
+    [DataContract]
+    public class Calc
+
+    {
+
+        [DataMember]
+        public double n1;
+
+        [DataMember]
+        public double n2;
+
+    }
+}
---
+++ ICalService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21WCFFSDCalculator
+{
+    [ServiceContract]
+    public interface ICalcService
+
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        [OperationContract]
+        double Divide(double n1, double n2);
+
+    }
+}
